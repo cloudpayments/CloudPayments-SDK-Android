6fea10a0e20805f67c7f55eb4a7daf0119a5fab5
a.ignatov <>
2022-10-25 13:18:50 +0500

    Fix error

--- sdk/src/main/java/ru/cloudpayments/sdk/ui/dialogs/ThreeDsDialogFragment.kt
+++ sdk/src/main/java/ru/cloudpayments/sdk/ui/dialogs/ThreeDsDialogFragment.kt
@@ -129,7 +129,7 @@ class ThreeDsDialogFragment : DialogFragment() {
 				} else {
 					listener?.onAuthorizationFailed(html ?: "")
 				}
-				dismiss()
+				dismissAllowingStateLoss()
 			}
 		}
 	}
